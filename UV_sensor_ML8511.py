71a0da9a07574f7af003f2ae6dd29d5c947d07cc
moufassih1996 <78668651+moufassih1996@users.noreply.github.com>
2021-04-05 23:57:56 +0100

    Add files via upload

---
+++ UV_sensor_ML8511.py
@@ -0,0 +1,25 @@
+from time import sleep
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+try:
+    i2c = busio.I2C(board.SCL, board.SDA)
+    ads = ADS.ADS1115(i2c)
+    chan = AnalogIn(ads, ADS.P2)
+except:
+    pass
+
+def voltage_to_UV(chan, in_min,  in_max,  out_min,  out_max)
+
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+def read_UV():
+    #those values are imported since dataShet 
+    try:
+        uvIntensity = mapfloat(outputVoltage, 0.99, 2.8, 0.0, 15.0); #Convert the voltage to a UV intensity level
+        return uvIntensity
+    except:
+        return None
+
